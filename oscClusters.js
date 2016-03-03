c79b075a1aefaf68724228a16a8536becd2279b0
Dave Koenig <davidjosephkoenig@gmail.com>
2016-03-03 00:43:24 -0500

    modularize

--- oscClusters.js
+++ oscClusters.js
@@ -1,47 +1,47 @@
-// make a bunch of harsh, randomized clusters of oscillators that never end
+var audioHelpers = (function(){
+	var context = new AudioContext();
+	OscillatorNode.prototype.WAVETYPES = {
+		0: 'sine',
+		1: 'square',
+		2: 'sawtooth',
+		3: 'triangle',
+		4: 'custom'
+	};
 
-function newOsc(audio, waveType, freq, duration, callback) {
-	var osc = audio.createOscillator();
-	osc.type = waveType;
-	osc.frequency.value = freq;
-	osc.connect(audio.destination);
-	osc.start();
-	setTimeout(function() {
-		osc.stop();
-		if (callback)
-			callback();
-	}, duration);
-}
+	return {
+		newOsc: function(waveType, freq, dur, cb){
+			// create a new oscillator
+			var osc = context.createOscillator();
+			osc.type = waveType;
+			osc.frequency.value = freq;
+			osc.connect(context.destination);
+			osc.start();
+			if (dur){
+				setTimeout(function(){
+					osc.stop();
+					cb ? cb() : null;
+				}, dur);
+			}
+			return osc;
+		},
 
-function oscCluster(baseFreq, totalOscs, freqSeparation, duration, callback) {
-	var cb = null;
-	for (var i = 0; i < totalOscs; i++) {
-		var waveType = OscillatorNode.prototype.waveTypes[i % 3];
-		var hz = freqSeparation * i + baseFreq;
-		if (i == (totalOscs - 1))
-			cb = callback;
-		newOsc(oscCluster.audio, waveType, hz, duration, cb);
-	}
-}
+		oscCluster: function(baseFreq, totalOscs, freqSep, dur, cb){
+			// create a cluster of new oscillators
+			for (var i = 0; i < totalOscs; i++){
+				var waveType = OscillatorNode.prototype.WAVETYPES[i % 3];
+				var freq = freqSep * i + baseFreq;
+				var callback = i === totalOscs - 1 ? cb : null;
+				this.newOsc(waveType, freq, dur, callback);
+			}
+		},
 
-OscillatorNode.prototype.WaveTypes = {
-	0: 'sine',
-	1: 'square',
-	2: 'sawtooth',
-	3: 'triangle',
-	4: 'custom'
-};
+		randInRange: function(min,max){
+			return Math.floor(Math.random() * (1 + max - min)) + min;
+		},
 
-oscCluster.audio = new AudioContext();
-
-function randInRange(min, max) {
-	return Math.floor(Math.random() * (1 + max - min)) + min;
-}
-
-function annoyingClusters() {
-	oscCluster(randInRange(50, 2000), 100, 50, randInRange(50, 200), annoyingClusters);
-}
-
-annoyingClusters();
-
-
+		annoyingClusters: function(){
+			// make annoying clusters that never end
+			this.oscCluster(this.randInRange(50, 2000), 100, 50, this.randInRange(50, 200), this.annoyingClusters.bind(this));
+		}
+	};
+})();
